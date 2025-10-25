0fdccbaad7962f53c768f6603c859a0b1c72276e
OjasKumar510 <ojas.kumar2024@vitstudent.ac.in>
2025-10-25 15:01:04 +0530

    Add files via upload

--- hello.py
+++ hello.py
@@ -13,22 +13,29 @@ angle=0
 count=0
 tempss=pos_x-20*math.cos(math.pi/2-angle)
 print("tempss ",tempss)
-keys={"Left":False,"Right":False,"Up":False,"Down":False,"Space":False}
+keys={"Left":False,"Right":False,"Up":False,"Down":False,"Stop":False}
 robo_img=pygame.image.load("roboss.png")
-def assign(ev,value,keys,counts):
-    if ev==pygame.K_LEFT:
-        keys["Left"]=value
-    elif ev==pygame.K_RIGHT:
-        keys["Right"]=value
-    elif ev==pygame.K_UP:
-        keys["Up"]=value
-        counts=1
-    elif ev==pygame.K_DOWN:
-        keys["Down"]=value
-        counts=-1
-    elif ev==pygame.K_SPACE:
-        keys["Space"]=value
-    return counts
+def get_command(keys,counts):
+#write your code here in this function include all the necessary terms but return the counts' updated value
+#returning counts is important for simulation
+
+
+
+
+#def assign(ev,value,keys,counts):
+    #if ev==pygame.K_LEFT:
+        #keys["Left"]=value
+    #elif ev==pygame.K_RIGHT:
+        #keys["Right"]=value
+    #elif ev==pygame.K_UP:
+        #keys["Up"]=value
+        #counts=1
+    #elif ev==pygame.K_DOWN:
+        #keys["Down"]=value
+        #counts=-1
+    #elif ev==pygame.K_SPACE:
+        #keys["Space"]=value
+    #return counts
     #assignCount(keys)
 #def assignCount(keys):
 #    if keys["Up"][0]==True:
@@ -41,7 +48,7 @@ def assignVelocity(keys,count):
         velocity_x=-1
     if keys["Up"]==True and count==1:
         velocity_x=1
-    if keys["Space"]==True:
+    if keys["Stop"]==True:
         velocity_x=0
     return velocity_x
 def assignAngle(keys,angles):
@@ -51,31 +58,38 @@ def assignAngle(keys,angles):
     elif keys["Left"]==True:
         angle+=5*(math.pi/180)
     return angle
-#def draw_obstacle(width):
-    #for i in range(5):
-        #pygame.draw.rect(sc,'green',(pos_x+width*i,420,30,30))
+#def Stop(keys):
+    #keys["Stop"]=False
+    #if keys["Up"]==True:
+        #keys["Up"]=False
+        #count=0
+        #keys["Up"][1]=0
+    #if keys["Down"]==True:
+        #keys["Down"]=False
+        #count=0
 while run:
     for ev in pygame.event.get():
         if ev.type==pygame.QUIT:
             run=False
-        elif ev.type==pygame.KEYDOWN:
-            count=assign(ev.key,True,keys,count)
-        elif ev.type==pygame.KEYUP:
-            if ev.key==pygame.K_SPACE:
-                keys["Space"]=False
-                if keys["Up"]==True:
-                    keys["Up"]=False
-                    count=0
+        #elif ev.type==pygame.KEYDOWN:
+            #count=assign(ev.key,True,keys,count)
+        #elif ev.type==pygame.KEYUP:
+            #if ev.key==pygame.K_SPACE:
+                #keys["Space"]=False
+                #if keys["Up"]==True:
+                    #keys["Up"]=False
+                    #count=0
                     #keys["Up"][1]=0
-                if keys["Down"]==True:
-                    keys["Down"]=False
-                    count=0
+                #if keys["Down"]==True:
+                    #keys["Down"]=False
+                    #count=0
                     #keys["Down"][1]=0
-            if ev.key==pygame.K_RIGHT:
-                keys["Right"]=False
-            if ev.key==pygame.K_LEFT:
-                keys["Left"]=False
+            #if ev.key==pygame.K_RIGHT:
+                #keys["Right"]=False
+            #if ev.key==pygame.K_LEFT:
+                #keys["Left"]=False
     #assign(ev.key,False,keys)
+    count=get_command(keys,count)
     clk.tick(10)
     print("count ",count)
     velocity_x=assignVelocity(keys,count)
@@ -90,17 +104,8 @@ while run:
     print("cos(angle): ",math.cos(math.pi/2))
     print("pos_x: ",pos_x)
     print(pos_y,temps)
-    #if temp!=pos_x :
-        #width=random.randint(50,80)
-    #if pos_x<5:
-    #    pos_x+=random.randint(400,500)
-    #if(pos_x>500):
-    #    pos_x=random.randint(0,10)
     clk.tick(80)
     sc.fill('orange')
-    #pygame.draw.rect(sc,'red',(0,450,500,50))
-    #pygame.draw.rect(sc,'yellow',(250,400,40,50))
-    #draw_obstacle(width)
     w=robo_img.get_width()
     h=robo_img.get_height()
     if pos_x<0:
